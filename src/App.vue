53f8918845183d203fe204b7821fc4515fbdc46f
RichardNzembei <richardsonreuben78@gmail.com>
2024-09-08 22:39:39 +0300

    UI update and  state management

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,137 @@
-<template>
-  <div>
-    <input v-model="newTodo" placeholder="Add a new task" />
-    <button @click="addTodo">add</button>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id">
-        {{ todo.text }}
-        <button @click="removeTodo(todo)">Delete</button>
-      </li>
-    </ul>
-  </div>
-</template>
-
 <script setup>
-import { ref } from "vue";
-
-const newTodo = ref("");
-const todos = ref([
-  { id: 1, text: "Learn Vue" },
-  { id: 2, text: "Get familiar with core concepts" },
-  { id: 2, text: "Build a To-Do App" },
-]);
-
-function addTodo() {
-  if (newTodo.value.trim()) {
-    todos.value.push({ id: Date.now(), text: newTodo });
-    newTodo = "";
+import { ref } from 'vue';
+const NewTodo=ref('')
+const todos=ref([])
+function addTodo(){
+  if(NewTodo.value.trim()){
+    todos.value.push({text:NewTodo.value})
+    NewTodo.value=""
   }
 }
-function removeTodo(todo) {
-  todos.value = todos.value.filter((t) => t !== todo);
-}
 </script>
+<template >
+  <div class="app">
+    <div class="todoApp">
+      <header class="header">
+        <h1>Task's</h1>
+      </header>
+      <section class="sectionCard">
+        <h2>Today's task   
+
+          
+        </h2>
+        <input  class="input" type="text" v-model="NewTodo" placeholder="add a task">
+        <button @click="addTodo">+ New task</button>
+        <section class="list">
+          <ul class="todos">
+            <li v-for="todo in todos" :key="todo.id">{{ todo.text}}</li>
+          </ul>
+
+        </section>
+        <br>
+      <section class="filters">
+        <ul>
+          <li>
+            <a href="#/All">All</a>
+          </li>
+          <li>
+            <a href="#/Open">Open</a>
+          </li>
+          <li>
+            <a href="#/Closed">Closed</a>
+          </li>
+          <li>
+            <a href="#/Archived">Archived</a>
+          </li>
+        </ul>
+     
+        
+      </section>
+          
+       
+
+      </section>
+    </div>
+  </div>
+  
+  
+   
+    
+  
+</template>
+<style scoped>
+.todoApp{
+  background-color: cornsilk;
+  margin: 130px 0  0;
+  justify-content: right;
+  padding: 20px 10px ;
+  max-width: 350px;
+  border-radius: 10px;
+  height:500px;
+}
+h1{
+  color: aqua;
+  font-display:inherit;
+  font-size: 50px;
+}
+.header{
+  text-decoration-line: underline;
+}
+button{
+  font-size: 150%;
+  position:fixed;
+  left: 220px;
+  border-radius: 5px;
+  size: 100px;
+  
+}
+h2{
+  align-content: space-around;
+}
+.filters {
+	
+	list-style: none;
+	
+	
+  background-color:darkgray
+   ;
+   max-width: 350px;
+   margin:  0  0;
+  justify-content: right;
+  padding: 20px 10px ;
+  border-radius: 3px;
+}
+
+.filters li {
+	display: inline;
+}
+
+.filters li a {
+	color: inherit;
+	margin: 3px;
+	padding: 3px 7px;
+	text-decoration: none;
+	border: 1px solid transparent;
+	border-radius: 3px;
+}
+
+.filters li a:hover {
+	border-color: #DB7676;
+}
+
+.filters li a.selected {
+	border-color: #CE4646;
+}
+.todos{
+  background-color: darkgray;
+  border-radius: 5px;
+
+}
+.input{
+  padding: 16px 16px 16px 16px;
+	height: 10px;
+	border: none;
+	background: #f1eaea;
+	box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
+}
+</style>
